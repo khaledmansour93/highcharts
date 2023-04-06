1ac603aff6ceb29c5b8fc459a1682613b25b22ce
Khaled Mansour <khaledmansour818@gmail.com>
2023-04-06 16:07:40 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Chart } from 'angular-highcharts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'highchartsApp';
+  chart = new Chart({
+    title: {
+      text: 'Combination chart'
+    },
+    xAxis: {
+      categories: ['Apples', 'Oranges', 'Pears', 'Bananas', 'Plums']
+    },
+    series: [
+      {
+        type: 'column',
+        name: 'Jane',
+        data: [3, 2, 1, 3, 4]
+      },
+      {
+        type: 'column',
+        name: 'John',
+        data: [2, 3, 5, 7, 6]
+      },
+      {
+        type: 'column',
+        name: 'Joe',
+        data: [4, 3, 3, 9, 0]
+      },
+      {
+        type: 'spline',
+        name: 'Average',
+        data: [3, 2.67, 3, 6.33, 3.33]
+      },
+      {
+        type: 'pie',
+        name: 'Total consumption',
+        data: [
+          {
+            name: 'Jane',
+            y: 13
+          },
+          {
+            name: 'John',
+            y: 23
+          },
+          {
+            name: 'Joe',
+            y: 19
+          }
+        ],
+        center: [100, 80],
+        size: 100,
+        showInLegend: false,
+        dataLabels: {
+          enabled: false
+        }
+      },
+    ]
+  });
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChartModule } from 'angular-highcharts';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
